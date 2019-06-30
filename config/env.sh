a64ee03b7ec355b713ba3321572fa6d5613b69d4
Fluke667 <Fluke667@gmail.com>
2019-07-01 00:46:13 +0200

    Update env.sh

--- config/env.sh
+++ config/env.sh
@@ -1,44 +1,51 @@
 #!/bin/sh
 
-
+export \
 # Basic Enviroment
-export GOROOT=/usr/lib/go
-export GOPATH=/go
-export PATH=/go/bin:$PATH
-export TZ=${TZ}
+GOROOT=/usr/lib/go \
+GOPATH=/go \
+PATH=/go/bin:$PATH \
+TZ=${TZ} \
 
 # shadowsocks-libev Enviroment
-export SS_LIBEV_VERSION=v3.2.5
-export KCP_VERSION=20190424
-export SS_DL=https://github.com/shadowsocks/shadowsocks-libev.git 
-export KCP_DL=https://github.com/xtaci/kcptun/releases/download/v${KCP_VERSION}/kcptun-linux-amd64-${KCP_VERSION}.tar.gz
-export PLUGIN_OBFS_DL=https://github.com/shadowsocks/simple-obfs.git
-export PLUGIN_V2RAY_DL=https://github.com/shadowsocks/v2ray-plugin/releases/download/v1.0/v2ray-plugin-linux-amd64-8cea1a3.tar.gz
-export PLUGIN_CLOAK_DL=https://github.com/cbeuw/cloak.git
+SS_LIBEV_VERSION=v3.2.5 \
+KCP_VERSION=20190424 \
+SS_DL=https://github.com/shadowsocks/shadowsocks-libev.git \
+KCP_DL=https://github.com/xtaci/kcptun/releases/download/v${KCP_VERSION}/kcptun-linux-amd64-${KCP_VERSION}.tar.gz \
+PLUGIN_OBFS_DL=https://github.com/shadowsocks/simple-obfs.git \
+PLUGIN_V2RAY_DL=https://github.com/shadowsocks/v2ray-plugin/releases/download/v1.0/v2ray-plugin-linux-amd64-8cea1a3.tar.gz \
+PLUGIN_CLOAK_DL=https://github.com/cbeuw/cloak.git \
 
 # TOR Relay Enviroment
-#export TOR_USER=MyUser
-export TOR_USER='MyUser'
-export TOR_NICK=MyNick
-export TOR_EMAIL=My@email.com
-export TOR_PASS=MyPass
-export TORSOCKS_USERNAME=MyUser
-export TORSOCKS_PASSWORD=MyPass
+TOR_USER=MyUser \
+TOR_NICK=MyNick \
+TOR_EMAIL=My@email.com \
+TOR_PASS=MyPass \
+TORSOCKS_USERNAME=MyUser \
+TORSOCKS_PASSWORD=MyPass \
 
 # sslh Enviroment
-export LISTEN_IP=0.0.0.0
-export LISTEN_PORT=443
-export HTTPS_HOST=127.0.0.1
-export HTTPS_PORT=8443
-export SSH_HOST=127.0.0.1
-export SSH_PORT=22
-export OPENVPN_HOST=127.0.0.1
-export OPENVPN_PORT=1194
-export TINC_HOST=127.0.0.1
-export TINC_PORT=655
-#export XMPP_HOST=127.0.0.1
-#export XMPP_PORT=5222
-#export ADB_HOST=127.0.0.1
-#export ADB_PORT=5037
-export SHADOWSOCKS_HOST=127.0.0.1
-export SHADOWSOCKS_PORT=8388
+LISTEN_IP=0.0.0.0 \
+LISTEN_PORT=443 \
+HTTPS_HOST=127.0.0.1 \
+HTTPS_PORT=8443 \
+SSH_HOST=127.0.0.1 \
+SSH_PORT=22 \
+OPENVPN_HOST=127.0.0.1 \
+OPENVPN_PORT=1194 \
+TINC_HOST=127.0.0.1 \
+TINC_PORT=655 \
+#XMPP_HOST=127.0.0.1 \
+#XMPP_PORT=5222 \
+#ADB_HOST=127.0.0.1 \
+#ADB_PORT=5037 \
+SHADOWSOCKS_HOST=127.0.0.1 \
+SHADOWSOCKS_PORT=8388 \
+
+# Stunnel Enviroment
+STUNNEL_CLIENT=no \
+STUNNEL_SERVICE=openvpn \
+STUNNEL_ACCEPT=0.0.0.0:4911 \
+STUNNEL_CONNECT=server:1194
+
+exec "$@"
