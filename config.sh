404427d46b16d47760d5342893d058a9b2acb2fc
Fluke667 <Fluke667@gmail.com>
2019-06-30 15:59:35 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo "**** install pip ****"
+echo "**** install+configure pip3 ****"
 if [ ! -e /usr/bin/python ]; then ln -sf python3 /usr/bin/python ; fi
-exec python3 -m ensurepip
-exec rm -r /usr/lib/python*/ensurepip
-exec pip3 install --no-cache --upgrade pip setuptools wheel
+rm -r /usr/lib/python*/ensurepip
 if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi
