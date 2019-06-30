a022f8a7926d26fd68e941913810de55345939ac
Fluke667 <Fluke667@gmail.com>
2019-06-30 17:01:25 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-echo "**** install+configure pip3 ****"
-if [ ! -e /usr/bin/python ]; then ln -sf python3 /usr/bin/python ; fi
-rm -r /usr/lib/python*/ensurepip
-if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi
+echo "**** Test ****"
