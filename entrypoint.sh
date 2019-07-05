eb9bda2ac829551143c7d0b292a9a89539f940ba
Fluke667 <Fluke667@gmail.com>
2019-07-06 00:08:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 set -e
 
-exec privoxy /etc/privoxy/config && ss-local -b 0.0.0.0 -u --fast-open -c /etc/shadowsocks-libev/privoxy.json
+#privoxy /etc/privoxy/config && ss-local -b 0.0.0.0 -u --fast-open -c /etc/shadowsocks-libev/privoxy.json
+
+sslocal -s $PRV_SERVER -p $PRV_SERVER_PORT -l $PRV_LOCAL_PORT -k $PRV_PASS -m $PRV_METHOLD -a $PRV_USER-d start -c $PRV_CONF 
+privoxy --no-daemon /etc/privoxy/config
+
+
