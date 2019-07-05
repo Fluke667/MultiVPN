54ad429e894feb327ece41752feca5a3b7a219b7
Fluke667 <Fluke667@gmail.com>
2019-07-05 22:56:39 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-      
-      
-        
-
-
-
-      
-      
-        
 set -e
+
+exec privoxy /etc/privoxy/config && ss-local -b 0.0.0.0 -u --fast-open -c /etc/shadowsocks-libev/privoxy.json
