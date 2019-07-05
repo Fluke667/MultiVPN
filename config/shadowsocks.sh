a0ce6a588a13977bc009823fca342e4629c5c477
Fluke667 <Fluke667@gmail.com>
2019-07-05 19:09:04 +0200

    Update shadowsocks.sh

--- config/shadowsocks.sh
+++ config/shadowsocks.sh
@@ -5,7 +5,7 @@ set -e
 exec ss-server -v \
     -s ${SS_SERVER_ADDR:-0.0.0.0} \
     -p ${SS_SERVER_PORT:-8388} \
-    -k ${SS_PASSWORD:-bob180180180} \
+    -k ${SS_PASSWORD:-MyPass} \
     -t ${SS_TIMEOUT:-300} \
     -m ${SS_METHOD:-chacha20} \
     -n ${SHADOWSOCKS_MAXOPENFILES:-1000} \
@@ -15,3 +15,16 @@ exec ss-server -v \
     -u \
     --plugin ${SS_PLUGIN} \
     --plugin-opts ${SS_PLUGIN_OPTS}
+&& kcpserver \
+    -l :${KCP_SERVER_ADDR:-0.0.0.0} \
+    -t 127.0.0.1:${KCP_SERVER_PORT:-9443} \
+    --key ${KCP_PASSWORD:-MyPass} \
+    --crypt ${KCP_ENCRYPT:-salsa20} \
+    --mode ${KCP_MODE:-fast2} \
+    --mtu ${KCP_MTU:-1400} \
+    --sndwnd ${KCP_SNDWND:-1024} \
+    --rcvwnd ${KCP_RCVWND:-1024} \
+    --dscp ${KCP_DSCP:-46} \
+    --datashard ${KCP_DATASHARD:-10} \
+    --parityshard ${KCP_PARITYSHARD:-0} \
+    --sockbuf ${KCP_SOCKBUF:-16777216}
