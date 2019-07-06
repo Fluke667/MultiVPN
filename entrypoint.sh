d7f31eb3123951e5714de45b1f78cfeccadb8b38
Fluke667 <Fluke667@gmail.com>
2019-07-06 11:55:56 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 #privoxy /etc/privoxy/config && ss-local -b 0.0.0.0 -u --fast-open -c /etc/shadowsocks-libev/privoxy.json
 
 ss-local -s $PRV_SERVER -p $PRV_SERVER_PORT -b $PRV_LOCAL -l $PRV_LOCAL_PORT -k $PRV_PASS -m $PRV_METHOLD -d start -c $PRV_CONF &
-privoxy --no-daemon /etc/privoxy/config
+privoxy --no-daemon /etc/privoxy/config &
+openvpn --config /etc/openvpn/server.conf
 
 
