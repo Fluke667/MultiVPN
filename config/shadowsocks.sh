d3b9bbb55e8807b281ab1423af3c0a5ada1145c4
Fluke667 <Fluke667@gmail.com>
2019-07-07 02:26:03 +0200

    Update shadowsocks.sh

--- config/shadowsocks.sh
+++ config/shadowsocks.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-exec ss-server -v \
+ss-server -v \
     -s ${SS_SERVER_ADDR:-0.0.0.0} \
     -p ${SS_SERVER_PORT:-8388} \
     -k ${SS_PASSWORD:-MyPass} \
     -t ${SS_TIMEOUT:-300} \
     -m ${SS_METHOD:-chacha20} \
     -n ${SS_MAXOPENFILES:-1000} \
-    -d ${SS_DNS:-1.1.1.1,1.0.0.1}
+    -d ${SS_DNS:-1.1.1.1,1.0.0.1} \
     --fast-open \
     --reuse-port \
     -u 
