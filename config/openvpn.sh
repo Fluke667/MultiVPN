6f07b626766b906c2c8a9034779f80132925de62
Fluke667 <Fluke667@gmail.com>
2019-07-13 12:00:25 +0200

    Update and rename 40.openvpn.sh to openvpn.sh

--- config/openvpn.sh
+++ config/openvpn.sh
@@ -6,12 +6,12 @@ mknod /dev/net/tun c 10 200
 IP=$(grep '^server .*$' /etc/openvpn/server.conf | awk '{print $2}')
 
 # first initialize easyrsa root keys and certificates:
-easyrsa3 init-pki
-easyrsa3 build-ca nopass
-easyrsa3 gen-req server nopass
+easyrsa init-pki
+easyrsa build-ca nopass
+easyrsa gen-req server nopass
 #easyrsa gen-req $1
-easyrsa3 sign-req server server
+easyrsa sign-req server server
 openssl verify -CAfile pki/ca.crt pki/issued/server.crt
-easyrsa3 gen-req vpn01de nopass
+easyrsa gen-req vpn01de nopass
 
 /usr/sbin/openvpn --cd /etc/openvpn --config /etc/openvpn/server.conf --script-security 2
