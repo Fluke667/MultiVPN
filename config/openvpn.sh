410a59ca493e751318cf3f1d39db59bd52090241
Fluke667 <Fluke667@gmail.com>
2019-07-13 07:17:25 +0200

    Update openvpn.sh

--- config/openvpn.sh
+++ config/openvpn.sh
@@ -3,8 +3,8 @@
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
 
-IP=$(grep '^server .*$' /etc/openvpn/server.conf | awk '{print $2}')
+IP=$(grep '^server .*$' /etc/openvpn/openvpn-server.conf | awk '{print $2}')
 
 #iptables -t nat -A POSTROUTING -s ${IP}/24 -o eth0 -j MASQUERADE
 
-/usr/sbin/openvpn --cd /etc/openvpn --config /etc/openvpn/server.conf --script-security 2
+/usr/sbin/openvpn --cd /etc/openvpn --config /etc/openvpn/openvpn-server.conf --script-security 2
