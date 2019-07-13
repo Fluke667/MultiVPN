08390b09325fc320fb5a79c8725fdca90f948343
Fluke667 <Fluke667@gmail.com>
2019-07-13 16:06:03 +0200

    Update openvpn.sh

--- config/openvpn.sh
+++ config/openvpn.sh
@@ -5,9 +5,5 @@ mknod /dev/net/tun c 10 200
 
 IP=$(grep '^server .*$' /etc/openvpn/server.conf | awk '{print $2}')
 
-#Generate Config Files
-#chmod 077 /etc/openvpn/vpnconf
-#/etc/openvpn/vpnconf server --server
-#/etc/openvpn/vpnconf client --client
 
 /usr/sbin/openvpn --cd /etc/openvpn --config /etc/openvpn/server.conf --script-security 2
