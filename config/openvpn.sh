05040a654fc1b3fdddde6563758a1c234fa2e6cb
Fluke667 <Fluke667@gmail.com>
2019-07-14 21:10:46 +0200

    Update openvpn.sh

--- config/openvpn.sh
+++ config/openvpn.sh
@@ -14,7 +14,38 @@ else
   echo "ENTRYPOINT: tlsauth.key already exists"       
 fi
     
-    
+  echo " ---> Generate Openvpn file ${CLIENT_NAME}-emb.ovpn"
+    echo client > ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    echo dev tun >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    echo proto udp >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    echo remote ${IP_ADDR} 1194 >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    echo cipher AES-256-CBC >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    echo auth SHA512 >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    #echo resolv-retry infinite >> ${CLIENT_NAME}.ovpn
+    #echo redirect-gateway def1 >> ${CLIENT_NAME}.ovpn
+    echo nobind >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    #echo comp-lzo yes >> ${CLIENT_NAME}.ovpn
+    echo persist-key >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    echo persist-tun >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+	  echo user openvpn >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+	  echo group openvpn >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    echo verb 3 >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    #echo setenv ALLOW_PASSWORD_SAVE 0 >> ${CLIENT_NAME}.ovpn 
+    #echo auth-user-pass >> ${CLIENT_NAME}.ovpn
+    echo '<ca>' >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    cat ${CRT_CERT_DIR}/${CRT_CA_NAME}.pem >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    echo '</ca>' >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    echo '<cert>' >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    cat ${CRT_CERT_DIR}/${CLIENT_NAME}.crt >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    echo '</cert>' >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    echo '<key>' >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    cat ${CRT_CERT_DIR}/${CLIENT_NAME}.key >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+    echo '</key>' >> ${OVPN_DIR}/${CLIENT_NAME}.ovpn
+
+  echo " ---> Generate Openvpn file ${CLIENT_NAME}-file.ovpn"
+
+
+
 
 #IP=$(grep '^server .*$' /etc/openvpn/server.conf | awk '{print $2}')
 
