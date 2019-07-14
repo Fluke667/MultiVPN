cfa45780a04c347a59cefd0e19a3bcd799061475
Fluke667 <Fluke667@gmail.com>
2019-07-14 20:38:54 +0200

    Update ssl.sh

--- config/ssl.sh
+++ config/ssl.sh
@@ -57,6 +57,9 @@ then
   echo " ---> Generate Diffie-Hellman Key"
   openssl dhparam \
     -out "$CRT_CERT_DIR/$CRT_DIFF_NAME-$CRT_DIFF_LENGTH.dh" $CRT_DIFF_LENGTH
+  # New Command:
+  #openssl genpkey -genparam -algorithm DH \
+  #  -out "$CRT_CERT_DIR/dhp4096.pem -pkeyopt dh_paramgen_prime_len:" $CRT_DIFF_LENGTH
     
 else
   echo "ENTRYPOINT: $CRT_ISSUER_NAME.crt already exists"
