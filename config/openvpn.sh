5c8ff9c658f1949a7639d8030d322c297e50fe53
Fluke667 <Fluke667@gmail.com>
2019-07-14 15:55:39 +0200

    Update openvpn.sh

--- config/openvpn.sh
+++ config/openvpn.sh
@@ -3,6 +3,19 @@
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
 
+if [ ! -f "$OVPN_TLSAUTH_KEY" ]
+then
+
+  echo " ---> Generate Openvpn TLS-Auth Key"
+  openvpn \
+    --genkey --secret "$OVPN_TLSAUTH_KEY"
+    
+else
+  echo "ENTRYPOINT: tlsauth.key already exists"       
+fi
+    
+    
+
 IP=$(grep '^server .*$' /etc/openvpn/server.conf | awk '{print $2}')
 
 
