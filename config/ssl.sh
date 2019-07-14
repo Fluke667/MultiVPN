00562c144b2e698ff99d7133cb5a3c5214595441
Fluke667 <Fluke667@gmail.com>
2019-07-14 13:48:32 +0200

    Update ssl.sh

--- config/ssl.sh
+++ config/ssl.sh
@@ -124,6 +124,26 @@ else
   echo "ENTRYPOINT: $CRT_KEYSTORE_NAME.pfx already exists"
 fi
 
+cat <<EOF > $CRT_CERT_DIR/certinfo.txt
+01. Root CA certificate Self-Signed
+Root CA Private key: $CRT_ROOT_NAME.key
+Root CA Certificate request: $CRT_ROOT_NAME.csr
+Root CA Certificate: $CRT_ROOT_NAME.crt
+02. Issuer certificate Self-Signed
+Issuer private key: $CRT_ISSUER_NAME.key
+Issuer Certificate request: $CRT_ISSUER_NAME.csr
+Issuer certificate: $CRT_ISSUER_NAME.crt
+03. Public certificate Signed by $CRT_ISSUER_CN
+Public RSA key: $CRT_PUBLIC_NAME.key
+Public Certificate request: $CRT_PUBLIC_NAME.csr
+Public Certificate : $CRT_PUBLIC_NAME.crt
+04. Other Things:
+Combine root and issuer Certificate: ca.pem
+PKCS12 keystore: $CRT_KEYSTORE_NAME.pfx
+
+EOF
+
+
 # run command passed to docker run
 exec "$@"
 
