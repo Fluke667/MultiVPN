6773f752a08060105e14eaa94aa301cb0c7aa0be
Fluke667 <Fluke667@gmail.com>
2019-07-14 02:12:18 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ ! -f "$OVPN_CA_CRT" ]
+then
+
 chmod 0777 /etc/openvpn/vpnconf
 cd /etc/openvpn && ./vpnconf server --server
 cd /etc/openvpn && ./vpnconf client --client
+
+else
+  echo "Openvpn Certificates already exists"
+fi
