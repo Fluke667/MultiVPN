b3ec927a8838f772bde239468285660b312ca48b
Fluke667 <Fluke667@gmail.com>
2019-07-14 11:50:07 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-#if [ ! -f $OVPN_CA_CRT ]
-if [ ! -f "/etc/openvpn/ca/ca.crt" ]
-then
-chmod 0777 /etc/openvpn/vpnconf
-cd /etc/openvpn && ./vpnconf server --server
-cd /etc/openvpn && ./vpnconf client --client
-else
-echo "Openvpn Certificates already exists"
-fi
+# run command passed to docker run      
+exec "$@"
