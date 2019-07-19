5081383a76984ce3a76ce74d79b552014d9afb0c
Fluke667 <Fluke667@gmail.com>
2019-07-20 00:31:19 +0200

    Update ssl.sh

--- config/ssl.sh
+++ config/ssl.sh
@@ -84,7 +84,7 @@ echo " ---> Later turn ON ... Slow for Testing"
   #openssl dhparam \
     #out ${CRT_DIFF}${CRT_DIFF_LENGTH}.pem ${CRT_DIFF_LENGTH}
    openssl dhparam \ 
-   -out /etc/certs/dhparam1024.pem 1024
+   out /etc/certs/dhparam1024.pem 1024
 
 
 
