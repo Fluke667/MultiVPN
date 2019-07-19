f3c26a48b19aa5dd1b3331cbfc48689254ad89ae
Fluke667 <Fluke667@gmail.com>
2019-07-19 22:39:25 +0200

    Update openvpn.sh

--- config/openvpn.sh
+++ config/openvpn.sh
@@ -68,7 +68,7 @@ echo " ---> Generate Openvpn file ${CLIENT_NAME}-file.ovpn"
     echo 'cert ${CLIENT_NAME}.crt' >> ${OVPN_DIR}/${CLIENT_NAME}-file.ovpn
     echo 'key ${CLIENT_NAME}.key' >> ${OVPN_DIR}/${CLIENT_NAME}-file.ovpn
     echo 'tls-auth ta.key 1' >> ${OVPN_DIR}/${CLIENT_NAME}-file.ovpn
-echo " ---> Generate Openvpn Config file "
+echo " ---> Generate openvpn.conf Config file "
     echo port 1194 >> ${OVPN_CONFIG}.conf
     echo proto udp >> ${OVPN_CONFIG}.conf
     echo dev tun >> ${OVPN_CONFIG}.conf
