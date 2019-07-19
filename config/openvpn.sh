bdcba602ffb6e61456f64d0913739f4469276105
Fluke667 <Fluke667@gmail.com>
2019-07-19 22:32:18 +0200

    Update openvpn.sh

--- config/openvpn.sh
+++ config/openvpn.sh
@@ -14,7 +14,7 @@ else
   echo "ENTRYPOINT: tlsauth.key already exists"       
 fi
     
-  echo " ---> Generate Openvpn file ${CLIENT_NAME}-emb.ovpn"
+echo " ---> Generate Openvpn file ${CLIENT_NAME}-emb.ovpn"
     echo client > ${OVPN_DIR}/${CLIENT_NAME}-emb.ovpn
     echo dev tun >> ${OVPN_DIR}/${CLIENT_NAME}-emb.ovpn
     echo proto udp >> ${OVPN_DIR}/${CLIENT_NAME}-emb.ovpn
@@ -45,9 +45,7 @@ fi
     echo '<tls-auth>' >> ${OVPN_DIR}/${CLIENT_NAME}-emb.ovpn
     cat ${OVPN_TLSAUTH_KEY} >> ${OVPN_DIR}/${CLIENT_NAME}-emb.ovpn
     echo '</tls-auth>' >> ${OVPN_DIR}/${CLIENT_NAME}-emb.ovpn
-    
-
-  echo " ---> Generate Openvpn file ${CLIENT_NAME}-file.ovpn"
+echo " ---> Generate Openvpn file ${CLIENT_NAME}-file.ovpn"
     echo client > ${OVPN_DIR}/${CLIENT_NAME}-file.ovpn
     echo dev tun >> ${OVPN_DIR}/${CLIENT_NAME}-file.ovpn
     echo proto udp >> ${OVPN_DIR}/${CLIENT_NAME}-file.ovpn
@@ -70,7 +68,26 @@ fi
     echo 'cert ${CLIENT_NAME}.crt' >> ${OVPN_DIR}/${CLIENT_NAME}-file.ovpn
     echo 'key ${CLIENT_NAME}.key' >> ${OVPN_DIR}/${CLIENT_NAME}-file.ovpn
     echo 'tls-auth ta.key 1' >> ${OVPN_DIR}/${CLIENT_NAME}-file.ovpn
-
+echo " ---> Generate Openvpn Config file "
+    echo port 1194 >> ${OVPN_CONFIG}.conf
+    echo proto udp >> ${OVPN_CONFIG}.conf
+    echo dev tun >> ${OVPN_CONFIG}.conf
+    echo ca /etc/certs/ca.crt >> ${OVPN_CONFIG}.conf
+    echo cert /etc/certs/server.crt >> ${OVPN_CONFIG}.conf
+    echo key /etc/certs/server.key >> ${OVPN_CONFIG}.conf
+    echo dh /etc/certs/dh4096.pem >> ${OVPN_CONFIG}.conf
+    echo tls-auth /etc/certs/ta.key 0 >> ${OVPN_CONFIG}.conf
+    echo server 10.8.0.0 255.255.255.0 >> ${OVPN_CONFIG}.conf
+    echo ifconfig-pool-persist ipp.txt >> ${OVPN_CONFIG}.conf #maintain record client/virtual IP
+    echo client-to-client >> ${OVPN_CONFIG}.conf
+    echo keepalive 10 120 >> ${OVPN_CONFIG}.conf
+    echo cipher AES-256-CBC >> ${OVPN_CONFIG}.conf
+    echo auth SHA512 >> ${OVPN_CONFIG}.conf
+    echo persist-key >> ${OVPN_CONFIG}.conf
+    echo persist-tun >> ${OVPN_CONFIG}.conf
+    echo status openvpn-status.log >> ${OVPN_CONFIG}.conf
+    echo verb 3 >> ${OVPN_CONFIG}.conf
+    echo explicit-exit-notify 1 >> ${OVPN_CONFIG}.conf
 
 
 /usr/sbin/openvpn --cd /etc/openvpn --config /etc/openvpn/openvpn.conf --script-security 2
