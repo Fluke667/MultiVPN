bead2872e8cd051e9d3a2b34c00a372f68cca586
Fluke667 <Fluke667@gmail.com>
2019-07-20 18:34:04 +0200

    Update tor.sh

--- config/tor.sh
+++ config/tor.sh
@@ -1,5 +1,39 @@
-#!/bin/bash
+#!/bin/sh
 set -o errexit
+
+echo " ---> Generate torrc Config File"
+    echo User tord > ${TOR_CONF}
+    echo Nickname MyName >> ${TOR_CONF}
+    echo ContactInfo noreply@mymail.com >> ${TOR_CONF}
+    echo DataDirectory /var/lib/tor >> ${TOR_CONF}
+    echo ORPort 9001 >> ${TOR_CONF}
+    echo #ORPort [IPv6-address]:9001 >> ${TOR_CONF}
+    echo #DirPort 9030 >> ${TOR_CONF}
+    echo ExitPolicy reject *:* >> ${TOR_CONF}
+    echo ExitPolicy reject6 *:* >> ${TOR_CONF}
+    echo SocksPort 9050 >> ${TOR_CONF}
+    echo #ControlSocket 0 >> ${TOR_CONF}
+    echo RelayBandwidthRate 1 MB >> ${TOR_CONF}
+    echo RelayBandwidthBurst 1 MB >> ${TOR_CONF}
+    echo AccountingMax 1 GBytes >> ${TOR_CONF}
+    echo ControlPort 9051 >> ${TOR_CONF}
+    echo HashedControlPassword $TOR_PASS >> ${TOR_CONF}
+    echo #CookieAuthentication 1 >> ${TOR_CONF}
+    echo #BridgeRelay 1 >> ${TOR_CONF}
+    echo #PublishServerDescriptor 0 >> ${TOR_CONF}
+echo " ---> Generate Torsocks Config File"
+    echo TorAddress 127.0.0.1 >> ${TOR_SOCKS}
+    echo TorPort 9050 >> ${TOR_SOCKS}
+    echo OnionAddrRange 127.42.42.0/24 >> ${TOR_SOCKS}
+    echo SOCKS5Username $TORSOCKS_USERNAME >> ${TOR_SOCKS}
+    echo SOCKS5Password $TORSOCKS_PASSWORD >> ${TOR_SOCKS}
+    echo #AllowInbound 1 >> ${TOR_SOCKS}
+    echo #AllowOutboundLocalhost 1 >> ${TOR_SOCKS}
+    echo #IsolatePID 1 >> ${TOR_SOCKS}
+
+
+
+
 
 chmodf() { find $2 -type f -exec chmod -v $1 {} \;
 }
