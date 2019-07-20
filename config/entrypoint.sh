522d7aa5f504d8d1899b659a5f08ce521dfaa745
Fluke667 <Fluke667@gmail.com>
2019-07-20 11:27:16 +0200

    Update entrypoint.sh

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-exec /config/openvpn.sh "$@" &
-exec /config/ssl.sh "$@" &
-exec /config/sshd.sh "$@"
+exec /config/openvpn.sh &
+exec /config/ssl.sh &
+exec /config/sshd.sh &
+exec "$@"
