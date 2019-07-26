c49f0e81cfc6cc0ec637b7274adacb7c49a40221
Fluke667 <Fluke667@gmail.com>
2019-07-26 19:59:52 +0200

    Delete pproxy.sh

--- config/pproxy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-pproxy -l socks4+socks5://:8090#$USER:$PASS &
-sleep 2 &
-pproxy -l http://:8080#$USER:$PASS &
-sleep 2 &
-exec pproxy -l ss://aes-256-gcm:yDRHHo1PjnolIVQHF3H4cpuL45udo7aHOco+Og==@:8070
-
-exec "$@"
