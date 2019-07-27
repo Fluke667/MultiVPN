7676ec60eae194a69d9219a24079e279c799031b
Fluke667 <Fluke667@gmail.com>
2019-07-27 02:00:42 +0200

    Update privoxy.sh

--- config/init/privoxy.sh
+++ config/init/privoxy.sh
@@ -21,13 +21,13 @@ EOF
 
 
 
-sed -i '/^listen-address.*/d' /usr/local/etc/privoxy/config
-echo "listen-address  ${PRV_SERVER}:${PRV_SERVER_PORT}" >> /usr/local/etc/privoxy/config
-echo "forward-socks5   /               ${PRV_LOCAL}:${PRV_LOCAL_PORT} ." >> /usr/local/etc/privoxy/config
-chown privoxy.privoxy /usr/local/etc/privoxy/*
+sed -i '/^listen-address.*/d' /etc/privoxy/config
+echo "listen-address  ${PRV_SERVER}:${PRV_SERVER_PORT}" >> /etc/privoxy/config
+echo "forward-socks5   /               ${PRV_LOCAL}:${PRV_LOCAL_PORT} ." >> /etc/privoxy/config
+chown privoxy.privoxy /etc/privoxy/*
 
 
-privoxy --no-daemon --user privoxy /usr/local/etc/privoxy/config &
+privoxy --user privoxy /etc/privoxy/config &
 #ss-local -b 0.0.0.0 -u --fast-open -c /etc/shadowsocks-libev/privoxy.json
 ss-local -s $PRV_SERVER -p $PRV_SERVER_PORT -k $PRV_PASS -m $PRV_METHOD -b $PRV_LOCAL -l $PRV_LOCAL_PORT -t $PRV_TIMEOUT --fast-open -u
 $@
