9463124c8dec8c894d502534bae0928524a07e10
Fluke667 <Fluke667@gmail.com>
2019-07-28 20:03:12 +0200

    Update kcptun_installer.sh

--- config/installer/kcptun_installer.sh
+++ config/installer/kcptun_installer.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+cat > /etc/shadowsocks-libev/kcptun.json <<EOF
+{
+        "listen": "${KCP_LISTEN}",
+        "target": "${KCP_TARGET}",
+        "key": "${KCP_KEY}",
+        "crypt": "${KCP_CRYPT}",
+        "mode": "${KCP_MODE}",
+        "mtu": ${KCP_MTU},
+        "sndwnd": ${KCP_SNDWND},
+        "rcvwnd": ${KCP_RCVWND},
+        "datashard": ${KCP_DATASHARD},
+        "parityshard": ${KCP_PARITYSHARD},
+        "dscp": ${KCP_DSCP},
+        "nocomp": ${KCP_NOCOMP},
+        "acknodelay": false,
+        "nodelay": 1,
+        "interval": 40,
+        "resend": 2,
+        "nc": 1,
+        "sockbuf": 16777217,
+        "smuxbuf": 16777217,
+        "keepalive": 10,
+        "pprof":false,
+        "quiet":false,
+        "tcp":false
+}  
+EOF
+
     cd ${DIR_TMP}
     tar zxf ${KCPTUN_FILE}-${KCPTUN_VER}.tar.gz
     mv server_linux_amd64 ${KCPTUN_DIR}
