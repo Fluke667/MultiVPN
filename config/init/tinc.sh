86c8b731a60a4c6131bb7094bcab3f573447c278
Fluke667 <Fluke667@gmail.com>
2019-08-02 18:34:13 +0200

    Update tinc.sh

--- config/init/tinc.sh
+++ config/init/tinc.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
                 #Create Folders
                 mkdir -p /etc/tinc/
-                mkdir -p /etc/tinc/hosts/
+		mkdir -p /etc/tinc/$TINC_NETNAME
+                mkdir -p /etc/tinc/$TINC_NETNAME/hosts
                 mkdir -p /var/log/tinc/
 
 
                 # Declare public and private IPs in the host file, CONFIG/NET/hosts/HOST
-                echo "Address = "$TINC_PUB_IP >> /etc/tinc/hosts/TINC_$NODE
-                echo "Subnet = "$TINC_PRIV_IP"/32" >> /etc/tinc/hosts/$TINC_NODE
-                echo "Cipher = id-aes256-GCM" >> /etc/tinc/hosts/$TINC_NODE
-                echo "Digest = whirlpool" >> /etc/tinc/hosts/$TINC_NODE
-                echo "MACLength = 16" >> /etc/tinc/hosts/$TINC_NODE
-                echo "Compression = "$TINC_COMPRESSION >> /etc/tinc/hosts/$TINC_NODE
+                echo "Address = "$TINC_PUB_IP >> /etc/tinc/$TINC_NETNAME/hosts/$TINC_NODE
+                echo "Subnet = "$TINC_PRIV_IP"/32" >> /etc/tinc/$TINC_NETNAME/hosts/$TINC_NODE
+                echo "Cipher = id-aes256-GCM" >> /etc/tinc/$TINC_NETNAME/hosts/$TINC_NODE
+                echo "Digest = whirlpool" >> /etc/tinc/$TINC_NETNAME/hosts/$TINC_NODE
+                echo "MACLength = 16" >> /etc/tinc/$TINC_NETNAME/hosts/$TINC_NODE
+                echo "Compression = "$TINC_COMPRESSION >> /etc/tinc/$TINC_NETNAME/hosts/$TINC_NODE
 
                 # Set Runtime Configuration for Tinc
-                echo "Name = "$TINC_NODE > /etc/tinc/tinc.conf
-                echo "AddressFamily = ipv4" >> /etc/tinc/tinc.conf
-                echo "Device = /dev/net/tun"  >> /etc/tinc/tinc.conf
-                echo "Interface = "$TINC_INTERFACE  >> /etc/tinc/tinc.conf
+                echo "Name = "$TINC_NODE > /etc/tinc/$TINC_NETNAME/tinc.conf
+                echo "AddressFamily = ipv4" >> /etc/tinc/$TINC_NETNAME/tinc.conf
+                echo "Device = /dev/net/tun"  >> /etc/tinc/$TINC_NETNAME/tinc.conf
+                echo "Interface = "$TINC_INTERFACE  >> /etc/tinc/$TINC_NETNAME/tinc.conf
 
                 # Edit the tinc-up script
-                echo '#!/bin/sh' >/etc/tinc/tinc-up
-                echo 'ifconfig '$TINC_INTERFACE $TINC_PRIV_IP' netmask 255.255.255.0' >> /etc/tinc/tinc-up
-                echo '#!/bin/sh' > /etc/tinc/tinc-down
-                echo 'ifconfig '$TINC_INTERFACE' down' >> /etc/tinc/tinc-down
-                chmod +x /etc/tinc/tinc-up
-                chmod +x /etc/tinc/tinc-down
+                echo '#!/bin/sh' >/etc/tinc/$TINC_NETNAME/tinc-up
+                echo 'ifconfig '$TINC_INTERFACE $TINC_PRIV_IP' netmask 255.255.255.0' >> /etc/tinc/$TINC_NETNAME/tinc-up
+                echo '#!/bin/sh' > /etc/tinc/$TINC_NETNAME/tinc-down
+                echo 'ifconfig '$TINC_INTERFACE' down' >> /etc/tinc/$TINC_NETNAME/tinc-down
+                chmod +x /etc/tinc/$TINC_NETNAME/tinc-up
+                chmod +x /etc/tinc/$TINC_NETNAME/tinc-down
 
 		
 		#Generate Certificate
