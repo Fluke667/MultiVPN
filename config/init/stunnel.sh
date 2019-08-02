5251ba5c6d6c4278c0007ce8e7937159d19161db
Fluke667 <Fluke667@gmail.com>
2019-08-03 00:59:02 +0200

    Create stunnel.sh

---
+++ config/init/stunnel.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+mkdir /etc/stunnel
+rm $STUNNEL_CONF
+
+cat > ${STUNNEL_CONF} <<-EOF
+cert = ${STUNNEL_CRT}
+key = ${STUNNEL_KEY}
+
+setuid = stunnel
+setgid = stunnel
+
+pid = /var/run/stunnel/stunnel.pid
+
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+
+CAfile = ${STUNNEL_CAFILE}
+verifyChain = ${STUNNEL_VERIFY_CHAIN}
+
+debug = ${STUNNEL_DEBUG}
+output = /var/log/stunnel/stunnel.log
+foreground = yes
+client = ${STUNNEL_CLIENT}
+
+[${STUNNEL_SERVICE}]
+accept = ${STUNNEL_ACCEPT}
+connect = ${STUNNEL_CONNECT}
+delay = ${STUNNEL_DELAY}
+
+EOF
+
+
+$@
