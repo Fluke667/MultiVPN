e6f181bd19fc5eba135048a7581487818396c312
Fluke667 <Fluke667@gmail.com>
2019-08-03 19:45:33 +0200

    Update firewall.sh

--- config/firewall.sh
+++ config/firewall.sh
@@ -2,23 +2,6 @@
 
 set -e
 
-# Sysctl Configs
-sysctl -e -q -w kernel.msgmnb=65536
-sysctl -e -q -w kernel.msgmax=65536
-sysctl -e -q -w kernel.shmmax=68719476736
-sysctl -e -q -w kernel.shmall=4294967296
-sysctl -e -q -w net.ipv4.ip_forward=1
-sysctl -e -q -w net.ipv4.conf.all.accept_source_route=0
-sysctl -e -q -w net.ipv4.conf.all.accept_redirects=0
-sysctl -e -q -w net.ipv4.conf.all.send_redirects=0
-sysctl -e -q -w net.ipv4.conf.all.rp_filter=0
-sysctl -e -q -w net.ipv4.conf.default.accept_source_route=0
-sysctl -e -q -w net.ipv4.conf.default.accept_redirects=0
-sysctl -e -q -w net.ipv4.conf.default.send_redirects=0
-sysctl -e -q -w net.ipv4.conf.default.rp_filter=0
-sysctl -e -q -w net.ipv4.conf.ens3.send_redirects=0
-sysctl -e -q -w net.ipv4.conf.ens3.rp_filter=0
-
 # configure firewall
 iptables -t nat -A POSTROUTING -s 10.99.99.0/24 ! -d 10.99.99.0/24 -j MASQUERADE
 iptables -A FORWARD -s 10.99.99.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
