b47d28b419f1e04a7da511635e28503b5ca750f5
Fluke667 <Fluke667@gmail.com>
2019-08-03 06:47:04 +0200

    Create installer.sh

---
+++ config/installer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+chmod u+x /config/installer/v2rayplug_installer.sh &
+chmod u+x /config/installer/kcptun_installer.sh &
+chmod u+x /config/installer/goquiet-installer.sh &
+chmod u+x /config/installer/cloak-installer.sh &
+/config/installer/v2rayplug_installer.sh &
+/config/installer/kcptun_installer.sh &
+/config/installer/goquiet-installer.sh &
+/config/installer/cloak-installer.sh
