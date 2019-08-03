dc462ae5f813efb64e5ea75943ef8ecc7405572c
Fluke667 <Fluke667@gmail.com>
2019-08-03 02:59:51 +0200

    Update tor.sh

--- config/tor.sh
+++ config/tor.sh
@@ -1,45 +1,46 @@
 #!/bin/sh
-set -o errexit
 
 echo " ---> Generate torrc Config File"
-         rm ${TOR_CONF}
+rm ${TOR_CONF}
+
 cat > ${TOR_CONF}<<-EOF
-           User $TOR_USER
-           #echo Nickname MyName
-           #ContactInfo noreply@mymail.com
-           DataDirectory /var/lib/tor
-           ORPort 9001
-           #ORPort [IPv6-address]:9001
-           #DirPort 9030
-           ExitPolicy reject *:*
-           ExitPolicy reject6 *:*
-           SocksPort 9050
-           #ControlSocket 0
-           RelayBandwidthRate 1 MB
-           RelayBandwidthBurst 1 MB
-           AccountingMax 1 GBytes
-           ControlPort 9051
-           HashedControlPassword $TOR_PASS
-           #CookieAuthentication 1
-           #BridgeRelay 1
-           #PublishServerDescriptor 0
+User $TOR_USER
+#echo Nickname MyName
+#ContactInfo noreply@mymail.com
+DataDirectory /var/lib/tor
+ORPort 9001
+#ORPort [IPv6-address]:9001
+#DirPort 9030
+ExitPolicy reject *:*
+ExitPolicy reject6 *:*
+SocksPort 9050
+#ControlSocket 0
+RelayBandwidthRate 1 MB
+RelayBandwidthBurst 1 MB
+AccountingMax 1 GBytes
+ControlPort 9051
+HashedControlPassword $TOR_PASS
+#CookieAuthentication 1
+#BridgeRelay 1
+#PublishServerDescriptor 0
 EOF
 
 echo " ---> Generate torsocks.conf Config File"
-         rm ${TOR_SOCKS}.conf
+rm ${TOR_SOCKS}.conf
+
 cat > ${TOR_SOCKS}<<EOF
-         TorAddress 127.0.0.1 
-         TorPort 9050
-         OnionAddrRange 127.42.42.0/24
-         SOCKS5Username $TORSOCKS_USER
-         SOCKS5Password $TORSOCKS_PASS
-         #AllowInbound 1
-         #AllowOutboundLocalhost 1
-         $COMMENT IsolatePID 1
-         $TOR_ADD meek exec $TOR_MEEK $TOR_OPT_MEEK
-         $TOR_ADD obfs3 exec $TOR_OBFS3 $TOR_OPT_OBFS3
-         $TOR_ADD obfs4 exec $TOR_OBFS4 $TOR_OPT_OBFS4
-         $TOR_ADD snowflake exec $TOR_SNOW $TOR_OPT_SNOW
+TorAddress 127.0.0.1 
+TorPort 9050
+OnionAddrRange 127.42.42.0/24
+SOCKS5Username $TORSOCKS_USER
+SOCKS5Password $TORSOCKS_PASS
+#AllowInbound 1
+#AllowOutboundLocalhost 1
+$COMMENT IsolatePID 1
+#$TOR_ADD meek exec $TOR_MEEK $TOR_OPT_MEEK
+#$TOR_ADD obfs3 exec $TOR_OBFS3 $TOR_OPT_OBFS3
+#$TOR_ADD obfs4 exec $TOR_OBFS4 $TOR_OPT_OBFS4
+#$TOR_ADD snowflake exec $TOR_SNOW $TOR_OPT_SNOW
 EOF
 
 echo -e "\n========================================================"
