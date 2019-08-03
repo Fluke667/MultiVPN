ab793e013359b9a5b2f696d0b4b67af3197ae14e
Fluke667 <Fluke667@gmail.com>
2019-08-03 18:50:07 +0200

    Update tor.sh

--- config/init/tor.sh
+++ config/init/tor.sh
@@ -31,57 +31,27 @@ EOF
 rm ${TOR_SOCKS}.conf
 
 echo " ---> Generate torsocks.conf Config File"
-cat > ${TOR_SOCKS}<<EOF
+cat > ${TORSOCKS_CONF_FILE}<<EOF
 TorAddress 127.0.0.1 
 TorPort 9050
 OnionAddrRange 127.42.42.0/24
-SOCKS5Username $TORSOCKS_USER
-SOCKS5Password $TORSOCKS_PASS
-#AllowInbound 1
+SOCKS5Username $TORSOCKS_USERNAME
+SOCKS5Password $TORSOCKS_PASSWORD
+AllowInbound $TORSOCKS_ALLOW_INBOUND
 #AllowOutboundLocalhost 1
-$COMMENT IsolatePID 1
+#IsolatePID 1
 #$TOR_ADD meek exec $TOR_MEEK $TOR_OPT_MEEK
 #$TOR_ADD obfs3 exec $TOR_OBFS3 $TOR_OPT_OBFS3
 #$TOR_ADD obfs4 exec $TOR_OBFS4 $TOR_OPT_OBFS4
 #$TOR_ADD snowflake exec $TOR_SNOW $TOR_OPT_SNOW
 EOF
 
-echo "\n========================================================"
+
 # If DataDirectory, identity keys or torrc is mounted here,
 # ownership needs to be changed to the TOR_USER user
 chown -Rv ${TOR_USER}:${TOR_USER} /var/lib/tor
 # fix permissions
 chmod -R 700 /var/lib/tor
 
-if [ ! -e /tor-config-done ]; then
-    touch /tor-config-done   # only run this once
-
-    # Add Nickname from env variable or randomized, if none has been set
-    if ! grep -q '^Nickname ' /etc/tor/torrc; then
-        if [ ${TOR_NICK} == "Tor4" ] || [ -z "${TOR_NICK}" ]; then
-            # if user did not change the default Nickname, genetrate a random pronounceable one
-            RPW=$(pwgen -0A 8)
-            TOR_NICK="Tor4${RPW}"
-            echo "Setting random Nickname: ${TOR_NICK}"
-        else
-            echo "Setting chosen Nickname: ${TOR_NICK}"
-        fi
-        echo "\nNickname ${TOR_NICK}" >> /etc/tor/torrc
-    fi
-
-    # Add TOR_EMAIL from env variable, if none has been set in torrc
-    if ! grep -q '^ContactInfo ' /etc/tor/torrc; then
-        # if TOR_EMAIL is not null
-        if [ -n "${TOR_EMAIL}" ]; then
-            echo "Setting Contact Email: ${TOR_EMAIL}"
-            echo "\nContactInfo ${TOR_EMAIL}" >> /etc/tor/torrc
-        fi
-    fi
-fi
-
-
-# execute from user
-#USER ${TOR_USER}
-#exec tor --RunAsDaemon 0 -f torrc
 
 "$@"
