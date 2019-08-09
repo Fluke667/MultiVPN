573f626d61e56f21d00391ef0da6a68ef883d1c9
Fluke667 <Fluke667@gmail.com>
2019-08-09 23:04:58 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,6 +27,8 @@ sslh-fork -f -u root --listen $SSLH_LISTEN --ssh $SSLH_SSH --tls $SSLH_TLS --ope
 pproxy -l socks4+socks5://:8010#$PPROXY_USER:$PPROXY_PASS &
 pproxy -l http://:8020#$PPROXY_USER:$PPROXY_PASS &
 pproxy -l ss://aes-256-gcm:yDRHHo1PjnolIVQHF3H4cpuL45udo7aHOco+Og==@:8030 &
+#i2pd --datadir=$DATA_DIR --reseed.verify=true --upnp.enabled=false --http.enabled=true --http.address=0.0.0.0 --httpproxy.enabled=true --httpproxy.address=0.0.0.0 --socksproxy.enabled=true --socksproxy.address=0.0.0.0 --sam.enabled=true --sam.address=0.0.0.0 &
+#peervpn /etc/peervpn/peervpn.conf &
 openvpn --writepid /run/openvpn/ovpn.pid --cd /etc/openvpn --config /etc/openvpn/openvpn.conf &
 proxybroker serve --host 0.0.0.0 --port 8888 --types HTTP HTTPS --lvl High &
 tinc --net=$TINC_NETNAME start --config=$TINC_DIR/$TINC_NETNAME -D --debug=$TINC_DEBUG --logfile=$TINC_LOG &
