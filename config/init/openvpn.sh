17f9fc920c8176db3ca4e49b925c1790b9856c1a
Fluke667 <Fluke667@gmail.com>
2019-08-10 04:54:59 +0200

    Update openvpn.sh

--- config/init/openvpn.sh
+++ config/init/openvpn.sh
@@ -7,10 +7,6 @@ mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
 chmod 0666 /dev/net/tun
 
-mkdir -p /etc/openvpn/easy-rsa/keys /var/log/openvpn /run/openvpn
-cp -r /usr/share/easy-rsa /etc/openvpn/
-ln -s /etc/openvpn/easy-rsa/easyrsa /usr/bin
-
 if [ ! -f $OVPN_TLSAUTH_KEY.key ]
 then
 
