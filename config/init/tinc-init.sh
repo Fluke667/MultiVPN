ae6b663d1f1992feaa0bec3ffb3fe31032cee68c
Fluke667 <Fluke667@gmail.com>
2019-08-17 19:04:31 +0200

    Update tinc-init.sh

--- config/init/tinc-init.sh
+++ config/init/tinc-init.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-                #Create Folders
-                mkdir -p /etc/tinc/ /etc/tinc/$TINC_NETNAME /etc/tinc/$TINC_NETNAME/hosts /var/log/tinc/
-
 
 		#Generate Certificate
 		tinc -n $TINC_NETNAME generate-keys 2048 < /dev/null
